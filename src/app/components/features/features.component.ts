8681445f171aaf38bea43e49b6442e1ec57a8927
Niels Jacobs <jacobsniels10@gmail.com>
2025-11-09 20:36:25 +0100

    all fixes

--- src/app/components/features/features.component.ts
+++ src/app/components/features/features.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeatureItem } from '../../types/HomePageData.types';
 
 @Component({
   selector: 'app-features',
   imports: [CommonModule],
   templateUrl: './features.component.html',
 })
-export class FeaturesComponent {}
+export class FeaturesComponent {
+  readonly title = input<string>('Onze waarden en normen');
+  readonly description = input<string>('FC landen draagt waarden en normen hoog in het vaandel.');
+  readonly featureItems = input<FeatureItem[]>([]);
+}
